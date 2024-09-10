2c8d68223e2343b6a43bc52df57808bddd4c2c69
Ehab Hussein <ehabsmh3@gmail.com>
2024-09-10 16:40:08 +0300

    Products Model implemented.

--- backend/src/models/Product.ts
+++ backend/src/models/Product.ts
@@ -1,34 +1,29 @@
-import { Document, Schema, Types, model } from "mongoose";
-
-// interface ProductDocument extends Document {
-
-// }
+import { Schema, Types, model } from "mongoose";
 
 const SizeSchema = new Schema({
   size: { type: String, enum: ['s', 'm', 'l', 'xl', 'xxl'], required: true },
   quantity: { type: Number, required: true },
   price: { type: Number, required: true },
   discountPrice: { type: Number, required: true },
-});
+}, { _id: false });
 
 const VariantSchema = new Schema({
-  color: { type: String, required: true, lowercase: true },
+  color: { type: String, required: true, lowercase: true, unique: true },
   colorCode: { type: String, required: true },
   images: [{ type: String, default: 'no image' }],
   sizes: [SizeSchema]
-})
+}, { _id: false });
 
 const ProductSchema = new Schema({
   name: { type: String, required: true },
   shortDescription: { type: String, required: true },
   longDescription: { type: String },
-  categoryId: { type: Types.ObjectId, ref: "Category" },
-  subcategoryId: { type: Types.ObjectId, ref: "Subcategory" },
+  categoryId: { type: Types.ObjectId, ref: "Category", required: true },
+  subcategoryId: { type: Types.ObjectId, ref: "Subcategory", required: true },
   slug: {
-    type: String, required: true, trim: true, lowercase: true,
-    set: (v: string) => {
-      v.replace(/\s+/g, "-").replace(/[^A-Za-z0-9]+/g, '-').replace(/[^A-Za-z0-9]+$/g, '');
-    }
+    type: String, trim: true, lowercase: true,
+    set: (v: string) =>
+      v.replace(/\s+/g, "-").replace(/[^A-Za-z0-9]+/g, '-').replace(/[^A-Za-z0-9]+$/g, '')
   },
   variants: [VariantSchema],
   sex: { type: String, enum: ['male', 'female', 'unisex'] },
@@ -36,6 +31,5 @@ const ProductSchema = new Schema({
 }, { timestamps: true })
 
 const Product = model('Product', ProductSchema);
+export default Product;
 
-
-// str1.replace(/\s+/g, "-").replace(/[^A-Za-z0-9]+/g,'-').replace(/[^A-Za-z0-9]+$/g, '')
