79c30c21ff7c328286ca78de03156eba03ea9b6a
Ehab Hussein <ehabsmh3@gmail.com>
2024-09-12 15:33:51 +0300

    product schema and its variants.

--- backend/dist/models/Product.js
+++ backend/dist/models/Product.js
@@ -1,6 +1,42 @@
 "use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
 Object.defineProperty(exports, "__esModule", { value: true });
+exports.VariantSchema = void 0;
 const mongoose_1 = require("mongoose");
+const ErrorHandler_1 = __importDefault(require("../utils/ErrorHandler"));
+const SizeSchema = new mongoose_1.Schema({
+    size: { type: String, enum: ['s', 'm', 'l', 'xl', 'xxl'], required: true, unique: true, immutable: true },
+    quantity: { type: Number, required: true },
+    price: { type: Number, required: true },
+    discountPrice: { type: Number, required: true },
+}, { _id: false });
+exports.VariantSchema = new mongoose_1.Schema({
+    color: { type: String, required: true, lowercase: true, unique: true },
+    colorCode: { type: String, required: true },
+    images: [{ type: String, default: 'no image' }],
+    sizes: {
+        type: [SizeSchema],
+        validate: {
+            validator: (sizes) => {
+                const sizeSet = new Set(sizes.map(s => s.size));
+                return sizeSet.size === sizes.length;
+            },
+            message: 'Sizes must be unique for each color variant.',
+            name: 'sizes',
+        }
+    }
+}, { _id: false });
 const ProductSchema = new mongoose_1.Schema({
     name: { type: String, required: true, unique: true },
     shortDesc: { type: String, required: true },
@@ -12,7 +48,19 @@ const ProductSchema = new mongoose_1.Schema({
         type: String, trim: true, lowercase: true,
         set: (v) => v.replace(/\s+/g, "-").replace(/[^A-Za-z0-9]+/g, '-').replace(/[^A-Za-z0-9]+$/g, '')
     },
+    variants: { type: [exports.VariantSchema], required: true },
     rating: { type: Number, default: 0, min: 0, max: 5 },
 }, { timestamps: true });
+ProductSchema.pre("findOneAndUpdate", function (next) {
+    return __awaiter(this, void 0, void 0, function* () {
+        console.log("xx");
+        this._update.variants.forEach((variant) => {
+            const sizeSet = new Set(variant.sizes.map(s => s.size));
+            if (sizeSet.size !== variant.sizes.length)
+                throw new ErrorHandler_1.default("Sizes must be unique for each color variant.", 400);
+        });
+        next();
+    });
+});
 const Product = (0, mongoose_1.model)('Product', ProductSchema);
 exports.default = Product;
--- backend/src/models/Product.ts
+++ backend/src/models/Product.ts
@@ -1,4 +1,38 @@
 import { Schema, Types, model } from "mongoose";
+import ErrorHandler from "../utils/ErrorHandler";
+import { VariantInterface } from "../interfaces/Product";
+
+type Size = {
+  size: string;
+  quantity: number;
+  price: number;
+  discountPrice: number;
+};
+
+const SizeSchema = new Schema({
+  size: { type: String, enum: ['s', 'm', 'l', 'xl', 'xxl'], required: true, unique: true, immutable: true },
+  quantity: { type: Number, required: true },
+  price: { type: Number, required: true },
+  discountPrice: { type: Number, required: true },
+}, { _id: false });
+
+export const VariantSchema = new Schema({
+  color: { type: String, required: true, lowercase: true, unique: true },
+  colorCode: { type: String, required: true },
+  images: [{ type: String, default: 'no image' }],
+  sizes: {
+    type: [SizeSchema],
+    validate: {
+      validator: (sizes: Size[]) => {
+        const sizeSet = new Set(sizes.map(s => s.size));
+        return sizeSet.size === sizes.length;
+      },
+      message: 'Sizes must be unique for each color variant.',
+      name: 'sizes',
+    }
+  }
+}, { _id: false });
+
 
 const ProductSchema = new Schema({
   name: { type: String, required: true, unique: true },
@@ -12,8 +46,19 @@ const ProductSchema = new Schema({
     set: (v: string) =>
       v.replace(/\s+/g, "-").replace(/[^A-Za-z0-9]+/g, '-').replace(/[^A-Za-z0-9]+$/g, '')
   },
+  variants: { type: [VariantSchema], required: true },
   rating: { type: Number, default: 0, min: 0, max: 5 },
 }, { timestamps: true })
+
+ProductSchema.pre("findOneAndUpdate", async function (next) {
+  console.log("xx");
+  this._update.variants.forEach((variant: VariantInterface) => {
+    const sizeSet = new Set(variant.sizes.map(s => s.size));
+    if (sizeSet.size !== variant.sizes.length) throw new ErrorHandler("Sizes must be unique for each color variant.", 400);
+  })
+  next();
+})
+
 
 const Product = model('Product', ProductSchema);
 export default Product;
